a8e31aab85db8fcdf6f1d9230a562726d9192cb3
DonHala <halapovaev@yandex.ru>
2024-03-11 19:27:31 +0500

    animation

--- Weather/View/Main/HomeView.swift
+++ Weather/View/Main/HomeView.swift
@@ -17,6 +17,7 @@ struct HomeView: View {
     @State private var isPresented = true
     @State private var selectedDetent: BottomSheet.PresentationDetent = .fraction(BottomSheetPosition.middle.rawValue)
     @State var bottomSheetTraslation : CGFloat = BottomSheetPosition.middle.rawValue
+    @State var hasDrag: Bool = false
     var bottomSheetTranlationProperty: CGFloat {
         (bottomSheetTraslation - BottomSheetPosition.middle.rawValue) / (BottomSheetPosition.top.rawValue - BottomSheetPosition.middle.rawValue)
     }
@@ -53,6 +54,7 @@ struct HomeView: View {
                             
                             Text("H:24°   L:18°")
                                 .font(.title3.weight(.semibold))
+                                .opacity(1 - bottomSheetTranlationProperty)
                         }
                         Spacer()
                     }
@@ -64,18 +66,24 @@ struct HomeView: View {
                     .sheetPlus(isPresented: $isPresented, background: Color.bottomSheetBackground.cornerRadius(44), onDrag: { translation in
                         bottomSheetTraslation = translation / screenHeight
                        // print(bottomSheetTranlationProperty.formatted())
-                 
+                        
+                        if selectedDetent == BottomSheet.PresentationDetent.fraction(0.9) {
+                            
+                            hasDrag = true
+                        } else {
+                            hasDrag = false
+                        }
             
                     }) {
                         ForecastView()
-                            .frame(height: 700)
-                            .presentationDetentsPlus([.height(385), .fraction(0.6), .medium, .fraction(1)], selection: $selectedDetent
+                            .frame(height: 600)
+                            .presentationDetentsPlus([.height(385), .fraction(0.6), .medium, .fraction(0.9)], selection: $selectedDetent
                             )
                     }
                     
                     // MARK: - Tab Bar
                     TabBar(action: {
-                        selectedDetent = .fraction(1)
+                        selectedDetent = .fraction(0.9)
                     })
                     .offset(y: bottomSheetTranlationProperty * 115)
                 }
@@ -85,16 +93,16 @@ struct HomeView: View {
     }
     
     private var attributedString: AttributedString {
-        var string = AttributedString("19°" + "\n " + "Чистое небо")
+        var string = AttributedString("19°" + (hasDrag ? " | " : "\n ") + "Чистое небо")
         
         if let temp = string.range(of: "19°") {
-            string[temp].font = .system(size: 96, weight: .thin)
-            string[temp].foregroundColor = .primary
+            string[temp].font = .system(size: (96 - (bottomSheetTranlationProperty * (80 - 20))), weight: .thin)
+            string[temp].foregroundColor = hasDrag ? .secondary : .primary
         }
         
         if let pipe = string.range(of: " | ") {
             string[pipe].font = .title3.weight(.semibold)
-            string[pipe].foregroundColor = .secondary
+            string[pipe].foregroundColor = .secondary.opacity(bottomSheetTranlationProperty)
         }
         
         if let weather = string.range(of: "Чистое небо") {
